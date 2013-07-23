664d21d1b76cf12b43cd0fc016ca273350241d1b
Ivan Avraamov <ivan.avraamov@gmail.com>
2013-07-23 16:01:46 +0300

    70% main page

--- desktop.bundles/index/index.bemjson.js
+++ desktop.bundles/index/index.bemjson.js
@@ -5,12 +5,13 @@
     head: [
         { elem: 'css', url: '_index.css' },
         { elem: 'css', url: '_index.ie.css', ie: 'gte IE 7' },
-        { elem: 'meta', attrs: { name: 'description', content: 'Благотворительный фонд «Город мечты» — проекты в области озеленения и благоустройства Симферополя, преобразования его архитектурного облика и повышения культурного уровня' }}
+		{ elem: 'css', url: 'http://fonts.googleapis.com/css?family=PT+Mono&subset=latin,cyrillic-ext,latin-ext,cyrillic' },
+		{ elem: 'meta', attrs: { name: 'description', content: 'Благотворительный фонд «Город мечты» — проекты в области озеленения и благоустройства Симферополя, преобразования его архитектурного облика и повышения культурного уровня' }}
         // , { elem: 'meta', attrs: { name: 'keywords', content: '' }}
     ],
     content:[
 		{
-			block: 'header',
+			elem: 'inner',
 			content: [
 				{
 					block: 'b-link',
@@ -19,10 +20,245 @@
 					content:
 					{
 						block: 'logo',
-						mods: {type: 'logo'}
+						mods: {type: 'logo'},
+						src: '/i/logo.png',
+						content: 'Город мечты'
 					}
+				},
+				{
+					block: 'nav',
+					content: [
+						{
+							elem: 'item',
+							content:
+							{
+								block: 'b-link',
+								url: '/about/',
+								content: "О фонде"
+							}
+						},
+						{
+							elem: 'item',
+							elemMods: { state: 'current'},
+							content: "Проекты"
+						},
+						{
+							elem: 'item',
+							content:
+							{
+								block: 'b-link',
+								url: '/news/',
+								content: "Новости"
+							}
+						},
+						{
+							elem: 'item',
+							content:
+							{
+								block: 'b-link',
+								url: '/partnership/',
+								content: "Сотрудничество"
+							}
+						},
+						{
+							elem: 'item',
+							content:
+							{
+								block: 'b-link',
+								url: '/cashflow/',
+								content: "Денежный поток"
+							}
+						}
+					]
+				},
+				{
+					block: 'discription',
+					content: [
+						{
+							elem: 'item',
+							content: 'Всех, кто разделяет нашу озабоченность будущим столицы Крыма и кто не хочет сидеть сложа руки, дожидаясь перемен, мы приглашаем к сотрудничеству.'
+						},
+						{
+							elem: 'item',
+							content: 'Предприятия, учреждения, общественные организации и просто неравнодушные люди – вместе мы сможем сделать наш любимый город настоящим Городом Мечты!'
+						}
+					]
+				},
+				{
+					block: 'b-link',
+					mods: { type: 'main-content-link'},
+					url: '/about/',
+					content: 'Наши проекты'
+				},
+				{
+					block: 'projects',
+					content: [
+						{
+							elem: 'item',
+							content: [
+								{
+									block: 'title',
+									content: [
+										'Сделано ',
+										{
+											block: 'b-link',
+											url: '/',
+											mods: { type: 'project-title-link'},
+											content: '(Х проектов)'
+										}
+									]
+								},
+								{
+									block: 'b-link',
+									url: '/',
+									mods: { type: 'italic-link'},
+									content: [
+										{
+											block: 'b-icon',
+											url: '../../../i/project-img-one.png',
+											alt: 'img',
+											mods: { type: 'project-link-img'}
+										},
+										'В ближайшем будущем  мы планируем взяться за освещение на улицах города'
+									]
+								}
+							]
+						},
+						{
+							elem: 'item',
+							content: [
+								{
+									block: 'title',
+									content: [
+										'Делаем ',
+										{
+											block: 'b-link',
+											url: '/',
+											mods: { type: 'project-title-link'},
+											content: '(Х проектов)'
+										}
+									]
+								},
+								{
+									block: 'b-link',
+									url: '/',
+									mods: { type: 'italic-link'},
+									content: [
+										{
+											block: 'b-icon',
+											url: '../../../i/project-img-two.png',
+											alt: 'img',
+											mods: { type: 'project-link-img'}
+										},
+										'Сейчас мы работаем над установкой площадок под мусорные баки'
+									]
+								}
+							]
+						},
+						{
+							elem: 'item',
+							content: [
+								{
+									block: 'title',
+									content: [
+										'Планы ',
+										{
+											block: 'b-link',
+											url: '/',
+											mods: { type: 'project-title-link'},
+											content: '(Х проектов)'
+										}
+									]
+								},
+								{
+									block: 'b-link',
+									url: '/',
+									mods: { type: 'italic-link'},
+									content: [
+										{
+											block: 'b-icon',
+											url: '../../../i/project-img-three.png',
+											alt: 'img',
+											mods: { type: 'project-link-img'}
+										},
+										'Мы установили 24 удобные скамейки в центральном парке'
+									]
+								}
+							]
+						}
+					]
+				},
+				{
+					block: 'banking',
+					content: [
+						{
+							elem: 'title',
+							content: [
+								{
+									elem: 'title-green',
+									content: 'Ты '
+								},
+								'тоже ',
+								{
+									elem: 'title-green',
+									content: 'можешь '
+								},
+								'помочь сделать мир ',
+								{
+									elem: 'title-green',
+									content: 'лучше!'
+								}
+							]
+						},
+						{
+							block: 'projects__item',
+							content: 'Перевести деньги',
+							mods: {type: 'min-title'}
+						},
+						{
+							block: 'regular-text',
+							content:[
+								'БО "Фонд Місто Мрії" в ',
+								{
+									block: 'b-link',
+									url: '/',
+									mods: { type: 'regular-link'},
+									content: 'Приватбанке'
+								}
+							]
+						},
+						{
+							block: 'formated-text',
+							content: 'Счет 	26001060922547'
+						},
+						{
+							block: 'formated-text',
+							content: 'ОКПО 	38711011'
+						},
+						{
+							block: 'formated-text',
+							content: 'МФО 	384436'
+						},
+						{
+							block: 'b-link',
+							url: '/',
+							mods: { type: 'block-link'},
+							content: 'Быть волонтёром'
+						},
+						{
+							block: 'br'
+						},
+						{
+							block: 'b-link',
+							url: '/',
+							mods: { type: 'block-link'},
+							content: 'Стать партнёром'
+						}
+					]
 				}
 			]
+
+
 		}
 //        {
 //            block: 'header',
