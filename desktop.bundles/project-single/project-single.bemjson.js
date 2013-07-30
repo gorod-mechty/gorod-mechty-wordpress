9219852b33bb5e500d6355bd316140d595d44c02
avraamov@tadatuta.com <ivan.avraamov@gmail.com>
2013-07-30 15:56:46 +0300

    project-single start

---
+++ desktop.bundles/project-single/project-single.bemjson.js
@@ -0,0 +1,162 @@
+({
+    block: 'b-page',
+    title: 'Симферополь — Город мечты',
+    favicon: '/favicon.ico',
+    head: [
+        { elem: 'css', url: '_project-single.css' },
+        { elem: 'css', url: '_project-single.ie.css', ie: 'gte IE 7' },
+        { elem: 'css', url: 'http://fonts.googleapis.com/css?family=PT+Mono&subset=latin,cyrillic-ext,latin-ext,cyrillic' },
+        { elem: 'meta', attrs: { name: 'description', content: 'Благотворительный фонд «Город мечты» — проекты в области озеленения и благоустройства Симферополя, преобразования его архитектурного облика и повышения культурного уровня' }}
+        // , { elem: 'meta', attrs: { name: 'keywords', content: '' }}
+    ],
+    content:[
+        {
+            elem: 'inner',
+            mix: {block: 'clearfix'},
+            content: [
+                {
+                    block: 'b-link',
+                    mix: {block: 'logo'},
+                    url: '/',
+                    content:""
+
+                },
+                {
+                    block: 'nav',
+                    content: [
+                        {
+                            elem: 'item',
+                            content:
+                            {
+                                block: 'b-link',
+                                url: '/about/',
+                                content: "О фонде"
+                            }
+                        },
+                        {
+                            elem: 'item',
+                            elemMods: { state: 'current'},
+                            content: "Проекты"
+                        },
+                        {
+                            elem: 'item',
+                            content:
+                            {
+                                block: 'b-link',
+                                url: '/news/',
+                                content: "Новости"
+                            }
+                        },
+                        {
+                            elem: 'item',
+                            content:
+                            {
+                                block: 'b-link',
+                                url: '/partnership/',
+                                content: "Сотрудничество"
+                            }
+                        },
+                        {
+                            elem: 'item',
+                            content:
+                            {
+                                block: 'b-link',
+                                url: '/cashflow/',
+                                content: "Денежный поток"
+                            }
+                        }
+                    ]
+                },
+                {
+                    block: 'content',
+                    content: [
+                        {
+                            block: 'heading_level_2',
+                            mods: { type: 'header'},
+                            content: 'Монтаж площадок под контейнеры'
+                        },
+                        {
+
+                        }
+                    ]
+                },
+                {
+                    block: 'left-nav',
+                    content:[
+                        {
+                            elem: 'item',
+                            content:
+                            {
+                                block: 'b-link',
+                                mods:{type: 'left-nav'},
+                                url: '/',
+                                content: "К списку проектов"
+                            }
+                        }
+                    ]
+                }
+
+
+
+            ]
+
+
+        },
+        {
+            block: 'footer',
+            mix: {block: 'clearfix'},
+            content: [
+                {
+                    block: 'text',
+                    mix: { block: 'footer', elem: 'text' },
+                    content: '© 2013  БО «Фонд Город Мечты»'
+                },
+                {
+                    block: 'b-link',
+                    url: 'mailto:info@gorod-mechty.org',
+                    mods: { type: 'footer-link'},
+                    content: 'info@gorod-mechty.org'
+                },
+                {
+                    block: 'socials',
+                    content:[
+                        {
+                            elem: 'social-item',
+                            content:[
+                                {
+                                    block: 'b-link',
+                                    url: '/',
+                                    mods: { type: 'vk'},
+                                    content: 'ВКонтакте'
+                                }
+                            ]
+                        },
+                        {
+                            elem: 'social-item',
+                            content:[
+                                {
+                                    block: 'b-link',
+                                    url: '/',
+                                    mods: { type: 'fb'},
+                                    content: 'FaceBook'
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    block: 'copyright',
+                    content:[
+                        'Создание сайта — ',
+                        {
+                            block: 'b-link',
+                            url: 'http://tadatuta.ru',
+                            mods: { type: 'normal'},
+                            content: 'студия «Тадатута»'
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+})
