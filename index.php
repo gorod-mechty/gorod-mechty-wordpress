3425cb1b2ef04cc3c55f6dc0ab78f68233a9a374
Ivan Avraamov <avraamov@tadatuta.com>
2013-08-22 16:30:03 +0000

    first php files added

---
+++ index.php
@@ -0,0 +1,125 @@
+<?php get_header(); ?>
+            <div class="description">
+                <p>
+                    Всех, кто разделяет нашу озабоченность будущим столицы Крыма и кто не хочет сидеть сложа руки, дожидаясь перемен, мы приглашаем к сотрудничеству.
+                </p>
+                <p>
+                    Предприятия, учреждения, общественные организации и просто неравнодушные люди - вместе мы сможем сделать наш любимый город настоящим Городом Мечты!
+                </p>
+            </div>
+            <h2 class="heading heading_level_2 main-heading">
+                <a class="b-link b-link_type_main" href="/projects/">Наши проекты</a>
+            </h2>
+            <div class="projects clearfix">
+                <div class="projects__item">
+                    <div class="projects__title">
+                        Сделано <a class="b-link" href="/">(Х проектов)</a>
+                    </div><a class="b-link b-link_type_project" href="/"><img class="b-icon" src="/i/project-img-one.png" alt="img">В ближайшем будущем мы планируем взяться за освещение на улицах города</a>
+                </div>
+                <div class="projects__item">
+                    <div class="projects__title">
+                        Делаем <a class="b-link" href="/">(Х проектов)</a>
+                    </div><a class="b-link b-link_type_project" href="/"><img class="b-icon" src="/i/project-img-two.png" alt="img">Сейчас мы работаем над установкой площадок под мусорные баки</a>
+                </div>
+                <div class="projects__item">
+                    <div class="projects__title">
+                        Планы <a class="b-link" href="/">(Х проектов)</a>
+                    </div><a class="b-link b-link_type_project" href="/"><img class="b-icon" src="/i/project-img-three.png" alt="img">Мы установили 24 удобные скамейки в центральном парке</a>
+                </div>
+            </div>
+            <div class="banking">
+                <div class="banking__title">
+                    <span class="banking__title-green">Ты</span> тоже <span class="banking__title-green">можешь</span> помочь сделать мир <span class="banking__title-green">лучше!</span>
+                </div>
+                <h4 class="heading heading_level_4">
+                    Перевести деньги
+                </h4>
+                <div class="text">
+                    БО «Фонд Місто Мрії» в <a class="b-link" href="/">Приватбанке</a>
+                </div>
+                <pre class="banking__details">
+Счет  26001060922547
+</pre>
+                <pre class="banking__details">
+ОКПО  38711011
+</pre>
+                <pre class="banking__details">
+МФО   384436
+</pre><a class="b-link b-link_type_block" href="/">Быть волонтёром</a>
+                <br>
+                <a class="b-link b-link_type_block" href="/">Стать партнёром</a>
+            </div>
+            <div class="news-list">
+                <h1 class="heading heading_level_1">
+                    Новости
+                </h1>
+                <div class="news-list__item">
+                    <div class="news-list__date">
+                        12 октября
+                    </div><a class="b-link b-link_style_italic" href="/">В Гаспре коммунальный пляж урезали в четыре раза</a>
+                </div>
+                <div class="news-list__item">
+                    <div class="news-list__date">
+                        12 октября
+                    </div><a class="b-link b-link_style_italic" href="/">В Севастополе второй день ищут гражданина Беларуси, унесенного в море во время шторма</a>
+                </div>
+                <div class="news-list__item">
+                    <div class="news-list__date">
+                        12 октября
+                    </div><a class="b-link b-link_style_italic" href="/">Севастополь предлагают лишить всех привилегий и сделать обычным админцентром «Таврической» области</a>
+                </div>
+                <div class="text news__more">
+                    Больше - <a class="b-link" href="/">в архиве</a>
+                </div>
+            </div>
+            <div class="cashflow">
+                <h1 class="heading heading_level_1">
+                    Денежный поток
+                </h1>
+                <div class="cashflow__item">
+                    <div class="title">
+                        ↓150 грн
+                    </div>
+                    <div class="cashflow__target">
+                        от
+                    </div>
+                    <div class="heading-text">
+                        Константина Константинопольского
+                    </div>
+                    <div class="cashflow__date">
+                        в 15:35, 07 июня 2013
+                    </div>
+                </div>
+                <div class="cashflow__item">
+                    <div class="title">
+                        ↑18200 грн
+                    </div>
+                    <div class="cashflow__target">
+                        на
+                    </div>
+                    <div class="heading-text">
+                        закупку материалов для установки скамеек в центральном парке
+                    </div>
+                    <div class="cashflow__date">
+                        в 15:35, 07 июня 2013
+                    </div>
+                </div>
+                <div class="cashflow__item">
+                    <div class="title">
+                        ↓1100 грн
+                    </div>
+                    <div class="cashflow__target">
+                        от
+                    </div>
+                    <div class="heading-text">
+                        Андрея М.
+                    </div>
+                    <div class="cashflow__date">
+                        в 15:35, 07 июня 2013
+                    </div>
+                </div>
+                <div class="text news__more">
+                    Показать <a class="b-link" href="/">весь список</a>
+                </div>
+            </div>
+ <?php get_footer(); ?>
---
+++ sidebar.php
@@ -0,0 +1,16 @@
+<div class="sidebar">
+                <a class="b-link logo" href="/"></a>
+                <ul class="submenu">
+                    <li class="submenu__item">
+                        <a class="b-link" href="/">Реквизиты</a>
+                    </li>
+                    <li class="submenu__item">Партнеры
+                    </li>
+                    <li class="submenu__item">
+                        <a class="b-link" href="/">Волонтёры</a>
+                    </li>
+                    <li class="submenu__item">
+                        <a class="b-link" href="/">Расходные материалы</a>
+                    </li>
+                </ul>
+            </div>
