7d1cceb562b85e3fbb5c83443493f57fc34bf689
Ivan Avraamov <avraamov@tadatuta.com>
2013-08-23 17:11:11 +0300

    start wp

---
+++ wordpress/wp-content/themes/gorod/footer.php
@@ -0,0 +1,17 @@
+<div class="footer">
+                <div class="footer__contacts">
+                    <div class="footer__text">
+                    © <?php echo date('Y'); ?> БО «Фонд Город Мечты»
+                    </div><a class="footer__link" href="mailto:info@gorod-mechty.org">info@gorod-mechty.org</a>
+                </div>
+                <div class="socials">
+                    <a class="socials__link socials__link_type_vk" href="/">ВКонтакте</a><a class="socials__link socials__link_type_fb" href="/">ВКонтакте</a>
+                </div>
+                <div class="copyright">
+                    Создание сайта - <a class="b-link" href="http://tadatuta.ru">студия «Тадатута»</a>
+                </div>
+            </div>
+        </div>
+        <?php wp_footer(); ?>
+    </body>
+</html>
---
+++ wordpress/wp-content/themes/gorod/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html class="i-ua_js_no i-ua_css_standard">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>
+            Симферополь - Фонд Город мечты
+        </title>
+        <link rel="icon" href="/favicon.ico">
+        <script>
+;(function(d,e,c,r){e=d.documentElement;c="className";r="replace";e[c]=e[c][r]("i-ua_js_no","i-ua_js_yes");if(d.compatMode!="CSS1Compat")e[c]=e[c][r]("i-ua_css_standart","i-ua_css_quirks")})(document);
+        </script>
+        <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>"><!--[if gte IE 7]><link rel="stylesheet" href="../merged/_merged.ie.css"/><![endif]-->
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=PT+Mono&amp;subset=latin,cyrillic-ext,latin-ext,cyrillic">
+        <meta name="description" content="Благотворительный фонд «Город мечты» — проекты в области озеленения и благоустройства Симферополя, преобразования его архитектурного облика и повышения культурного уровня">
+        <?php wp_head(); ?>
+    </head>
+    <body class="b-page b-page__body b-page_page_main">
+        <div class="b-page__inner b-page__inner_state_main clearfix">
+            <div class="logo"></div>
+            <ul class="nav">
+                <li class="nav__item">
+                    <a class="b-link b-link_type_nav" href="/about/">О фонде</a>
+                </li>
+                <li class="nav__item">
+                    <a class="b-link b-link_type_nav" href="/projects/">Проекты</a>
+                </li>
+                <li class="nav__item">
+                    <a class="b-link b-link_type_nav" href="/news/">Новости</a>
+                </li>
+                <li class="nav__item">
+                    <a class="b-link b-link_type_nav" href="/partners/">Сотрудничество</a>
+                </li>
+                <li class="nav__item">
+                    <a class="b-link b-link_type_nav" href="/cashflow/">Денежный поток</a>
+                </li>
+            </ul>
