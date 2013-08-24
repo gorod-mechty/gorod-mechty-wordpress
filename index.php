a1f3ca237c6f8166ed28eaef6b19e8f8861517e3
Ivan Avraamov <avraamov@tadatuta.com>
2013-08-24 23:17:23 +0300

    index.php fix

--- index.php
+++ index.php
@@ -3,7 +3,27 @@
                 <?php if (is_front_page()){ ?>
                     <?php include(TEMPLATEPATH . '/frontpage.php'); ?>
                 <?php } else{ ?>
-                    <div class="test">1</div>
+                    <?php if ( is_category(1)) { ?>
+                    <div class="content">
+                        <h1 class="heading heading_level_1">Новости</h1>
+                        <ul class="news">
+                            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                                <li class="news__item" id="post-<?php the_ID(); ?>">
+                                    <div class="news__date">
+                                        <?php the_time('j F'); ?>
+                                    </div><a class="b-link b-link_style_italic" href="<?php the_permalink() ?>"><?php the_post_thumbnail(); ?><?php the_title(); ?></a>
+                                </li>
+                            <?php endwhile; else: ?>
+                                <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+                            <?php endif; ?>д
+                            <?php posts_nav_link(' &#8212; ', __('&laquo; Newer Posts'), __('Older Posts &raquo;')); ?>
+                        </ul>
+                    </div>
+                    <?php } ?>
+                    <?php endwhile; else: ?>
+                        <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+                    <?php endif; ?>
+                    <?php posts_nav_link(' &#8212; ', __('&laquo; Newer Posts'), __('Older Posts &raquo;')); ?>
                 <?php } ?>
             </div>
 <?php get_footer(); ?>
