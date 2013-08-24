5d507dbbd00bdfd3b9666ea0bb1cfbec3f550112
Ivan Avraamov <avraamov@tadatuta.com>
2013-08-24 16:02:44 +0300

    fix index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
                 <?php if (is_front_page()){ ?>
                     <?php include(TEMPLATEPATH . '/frontpage.php'); ?>
                 <?php } else{ ?>
-                    <?php if ( in_category('1') { ?>
+                    <?php if ( is_category('1') { ?>
                     <div class="content">
                         <h1 class="heading heading_level_1">Новости</h1>
                         <ul class="news">
@@ -11,13 +11,8 @@
                                 <li class="news__item" id="post-<?php the_ID(); ?>">
                                     <div class="news__date">
                                         <?php the_time('j F'); ?>
-                                    </div>
-                                    <a class="b-link b-link_style_italic" href="<?php the_permalink() ?>"><?php the_post_thumbnail(); ?><?php the_title(); ?></a>
+                                    </div><a class="b-link b-link_style_italic" href="<?php the_permalink() ?>"><?php the_post_thumbnail(); ?><?php the_title(); ?></a>
                                 </li>
-                            <?php endwhile; else: ?>
-                                <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
-                            <?php endif; ?>
-                            <?php posts_nav_link(' &#8212; ', __('&laquo; Newer Posts'), __('Older Posts &raquo;')); ?>
                             <?php endwhile; else: ?>
                                 <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
                             <?php endif; ?>
@@ -25,5 +20,10 @@
                         </ul>
                     </div>
                     <?php } ?>
+                    <?php endwhile; else: ?>
+                        <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+                    <?php endif; ?>
+                    <?php posts_nav_link(' &#8212; ', __('&laquo; Newer Posts'), __('Older Posts &raquo;')); ?>
+                <?php } ?>
             </div>
 <?php get_footer(); ?>
