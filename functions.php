70c3a3ffeddb45978b8c4b6544707b45ebeb5e26
Ivan Avraamov <avraamov@tadatuta.com>
2013-08-27 18:26:45 +0300

    functions.php fix-2

--- functions.php
+++ functions.php
@@ -1,7 +1,6 @@
 <?php add_theme_support('post-thumbnails'); ?>
 
-function my_theme_register_menus()
-{
-    register_nav_menu('submenu', 'submenu');
+function register_my_menu() {
+  register_nav_menu('header-menu',__( 'Header Menu' ));
 }
-add_action( 'init', 'my_theme_register_menus');
+add_action( 'init', 'register_my_menu' );
