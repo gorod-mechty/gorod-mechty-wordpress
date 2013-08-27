607e3453b8d3ef73cc6c3ad5a3f9d0f901ae9cc5
Ivan Avraamov <avraamov@tadatuta.com>
2013-08-27 15:04:02 +0300

    add single news template

--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,19 @@
 <div class="sidebar">
                 <a class="b-link logo" href="/"></a>
                 <ul class="submenu">
+
+                <?php if (is_front_page()){ ?>
+                    <?php  
+                        global $post;  
+                            $args = array( 'numberposts' => -1, 'category' => 6 );  
+                            $myposts = get_posts( $args );  
+                        foreach( $myposts as $post ) :  setup_postdata($post); ?>  
+                             <li class="submenu__item">
+                                <a class="b-link"> href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                             </li>  
+                        <?php endforeach; ?>
+                    ?>
+                <?php } else{ ?>
                     <li class="submenu__item">
                         <a class="b-link" href="/">Реквизиты</a>
                     </li>
@@ -12,5 +25,6 @@
                     <li class="submenu__item">
                         <a class="b-link" href="/">Расходные материалы</a>
                     </li>
+                <?php } ?>
                 </ul>
             </div>
---
+++ single-news.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Template Name Posts: Single News
+*/
+?>
+<?php get_header(); ?>
+    <div class="content">
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+        <h1 class="heading heading_level_1"><?php the_title(); ?></h1>
+        <div class="news-single">
+            <div class="date"><?php the_time('j F'); ?></div>
+            <?php
+            the_content(__('(more...)'));
+            wp_link_pages();
+            edit_post_link(__('Edit This'));
+            ?>
+        </div>
+    <?php endwhile; else: ?>
+        <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+    <?php endif; ?>
+        <?php posts_nav_link(' &#8212; ', __('&laquo; Newer Posts'), __('Older Posts &raquo;')); ?>
+    </div>
+<?php get_footer(); ?>
