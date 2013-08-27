b5dbc9b49256d8555c0fd27d1d9166b8af42a580
Ivan Avraamov <avraamov@tadatuta.com>
2013-08-27 18:20:34 +0300

    add menus support

--- functions.php
+++ functions.php
@@ -1,1 +1,6 @@
 <?php add_theme_support('post-thumbnails'); ?>
+function add_munus()
+{
+    register_nav_menu('submenu',__( 'submenu' ));
+}
+add_action( 'init', 'add_menus');
--- sidebar.php
+++ sidebar.php
@@ -1,19 +1,7 @@
 <div class="sidebar">
                 <a class="b-link logo" href="/"></a>
                 <ul class="submenu">
-
-                <?php if (is_front_page()){ ?>
-                    <?php  
-                        global $post;  
-                            $args = array( 'numberposts' => -1, 'category' => 6 );  
-                            $myposts = get_posts( $args );  
-                        foreach( $myposts as $post ) :  setup_postdata($post); ?>  
-                             <li class="submenu__item">
-                                <a class="b-link"> href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                             </li>  
-                        <?php endforeach; ?>
-                    ?>
-                <?php } else{ ?>
+                    wp_nav_menu
                     <li class="submenu__item">
                         <a class="b-link" href="/">Реквизиты</a>
                     </li>
