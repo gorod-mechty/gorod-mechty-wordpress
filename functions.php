51a36ce5d6906e9f013a3bc700012315689f305b
Ivan Avraamov <avraamov@tadatuta.com>
2013-08-27 18:33:56 +0300

    functions.php fix-3

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php add_theme_support('post-thumbnails'); ?>
 
 function register_my_menu() {
-  register_nav_menu('header-menu',__( 'Header Menu' ));
+  register_nav_menu('header-menu',__( 'Header Menu', 'gorod-mechty-www' ));
 }
 add_action( 'init', 'register_my_menu' );
