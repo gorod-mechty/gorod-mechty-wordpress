53ac71aa1add5cd985e6ac143b5c099aa146feaa
Ivan Avraamov <avraamov@tadatuta.com>
2013-08-28 15:29:52 +0300

    arhive page template fix-5

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
                     <?php if ( is_category(1)|| is_category(6)|| is_category(7)) { ?>
                         <div class="content">
                             <h1 class="heading heading_level_1"><?php $cat = get_the_category(); echo $cat[0]->cat_name; ?></h1>
-                            <?php if ( is_category(1)|| is_archive() { ?>
+                            <?php if ( is_category(1)|| is_archive()) { ?>
                                 <ul class="news">
                                     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
                                         <li class="news__item" id="post-<?php the_ID(); ?>">
