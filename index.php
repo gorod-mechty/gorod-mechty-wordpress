13c37df99fe6c5fdcee192da199f2347b31c0a5a
Ivan Avraamov <avraamov@tadatuta.com>
2013-09-03 13:36:03 +0300

    projects substr test fix-10

--- index.php
+++ index.php
@@ -169,7 +169,11 @@
                                             <img class="b-icon projects-columns__img-l" src="<?php echo $image_l[0]; ?>" />
                                             <img class="b-icon projects-columns__img-r" src="<?php echo $image_r[0]; ?>" />
                                         </div>
-                                        <a class="b-link" href="<?php the_permalink() ?>"><?php echo rtrim(mb_substr(get_the_title(), 0, 45)) . '&hellip;'; ?></a>
+                                        <a class="b-link" href="<?php the_permalink() ?>">
+                                            <?php $title = get_the_title();
+                                            $trimmed_title = rtrim(mb_substr($title, 0, 20));
+                                            echo mb_strlen($title) > mb_strlen($trimmed_title) ? $trimmed_title . '&hellip;' : $title; ?>
+                                        </a>
                                         <div class="text text_style_italic"><?php the_field('comment'); ?></div>
                                     </div>
                                 <?php endforeach; ?>
