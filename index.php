07adffe61c2af07ab5d0e7d542130758cc8b71eb
Ivan Avraamov <avraamov@tadatuta.com>
2013-09-03 12:01:16 +0300

    projects substr fix

--- index.php
+++ index.php
@@ -117,7 +117,7 @@
                                             <img class="b-icon projects-columns__img-l" src="<?php echo $image_l[0]; ?>" />
                                             <img class="b-icon projects-columns__img-r" src="<?php echo $image_r[0]; ?>" />
                                         </div>
-                                        <a class="b-link" href="<?php the_permalink() ?>"><?php the_title(); ?></a>
+                                        <a class="b-link" href="<?php the_permalink() ?>"><?php substr(the_title(), 0, 20) . '&hellip;' ?></a>
                                         <div class="text text_style_italic"><?php the_field('comment'); ?></div>
                                     </div>
                                 <?php endforeach; ?>
