a34d2d780f79c1f4c185bd44e06b138fd13eae9d
Ivan Avraamov <avraamov@tadatuta.com>
2013-09-10 18:31:50 +0300

    test with get_field()-15

--- sidebar.php
+++ sidebar.php
@@ -19,11 +19,10 @@
                 <ul class="menu">
                     <li class="submenu__item">Новости проекта</li>
                     <?php $news = get_field('project_news'); ?>
-                    <?php  date('y-n-d', strtotime($news_item->post_date))
-                    ?>
                     <?php print_r($news) ?>
                         <?php
                             foreach ($news as $news_item) {
+                                echo date('y-n-d', strtotime($news_item->post_date));
                                 echo '<li class="submenu__item">'.'<span>'. $news_item->post_date .'</span>'.'<a class="b-link" href="' . home_url() . "/" . $news_item->post_name . '">' . $news_item->post_title . '</a></li>';
                             }
                             unset($news_item);
