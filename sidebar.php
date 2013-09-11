ae72654819d800d32dc0d9adfef7145fe62ffbf3
Ivan Avraamov <avraamov@tadatuta.com>
2013-09-11 15:06:00 +0300

    sidebar check for project news

--- sidebar.php
+++ sidebar.php
@@ -5,27 +5,29 @@
                 <?php } else if(in_category(6) || is_page(104) || is_page(109) || is_page(111)){ ?>
                     <?php wp_nav_menu( array( 'theme_location' => 'partners' ) ); ?> 
                 <?php } else if(in_category(1)){ ?>
-                <ul class="menu">
-                    <li class="submenu__item">
-                        <a class="b-link" href="<?php echo site_url(); ?>/?cat=1">К списку новостей</a>
-                    </li>
-                </ul>
+                    <ul class="menu">
+                        <li class="submenu__item">
+                            <a class="b-link" href="<?php echo site_url(); ?>/?cat=1">К списку новостей</a>
+                        </li>
+                    </ul>
                 <?php } else if(in_category(2)){ ?>
-                <ul class="menu">
-                    <li class="submenu__item">
-                        <a class="b-link" href="<?php echo site_url(); ?>/?cat=2">К списку проектов</a>
-                    </li>
-                </ul>
-                <ul class="menu">
-                    <li class="submenu__item">Новости проекта</li>
-                    <?php $news = get_field('project_news'); ?>
-                        <?php
-                            foreach ($news as $news_item) {
-                                echo '<li class="submenu__item">'.'<span class="submenu__data">'. date('d-m-Y', strtotime($news_item->post_date)) .'</span>'.'<a class="b-link" href="' . home_url() . "/" . $news_item->post_name . '">' . $news_item->post_title . '</a></li>';
-                            }
-                            unset($news_item);
-                        ?>
-                </ul>
+                    <ul class="menu">
+                        <li class="submenu__item">
+                            <a class="b-link" href="<?php echo site_url(); ?>/?cat=2">К списку проектов</a>
+                        </li>
+                    </ul>
+                    if(get_field('project_news')){
+                        <ul class="menu">
+                        <li class="submenu__item">Новости проекта</li>
+                        <?php $news = get_field('project_news'); ?>
+                            <?php
+                                foreach ($news as $news_item) {
+                                    echo '<li class="submenu__item">'.'<span class="submenu__data">'. date('d-m-Y', strtotime($news_item->post_date)) .'</span>'.'<a class="b-link" href="' . home_url() . "/" . $news_item->post_name . '">' . $news_item->post_title . '</a></li>';
+                                }
+                                unset($news_item);
+                            ?>
+                        </ul>
+                    }
                 <?php } else{ ?>
                 
                 <?php } ?>
