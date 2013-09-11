ef0b5ce5cf32d2c292636230fc007546c18eb8d2
Ivan Avraamov <avraamov@tadatuta.com>
2013-09-11 14:58:18 +0300

    link fix for projects

--- index.php
+++ index.php
@@ -151,9 +151,8 @@
                                             $image_l = wp_get_attachment_image_src( $left_img, $size ); 
                                             $image_r = wp_get_attachment_image_src( $right_img, $size );
                                             ?>
-
-                                            <img class="b-icon projects-columns__img-l" src="<?php echo $image_l[0]; ?>" />
-                                            <img class="b-icon projects-columns__img-r" src="<?php echo $image_r[0]; ?>" />
+                                            <a href="<?php the_permalink() ?>"><img class="b-icon projects-columns__img-l" src="<?php echo $image_l[0]; ?>"/></a>
+                                            <a href="<?php the_permalink() ?>"><img class="b-icon projects-columns__img-r" src="<?php echo $image_r[0]; ?>"/></a>
                                         </div>
                                         <a class="b-link" href="<?php the_permalink() ?>"><a class="b-link" href="<?php the_permalink() ?>">
                                             <?php $title = get_the_title();
