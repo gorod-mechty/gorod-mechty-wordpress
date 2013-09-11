71aa99cdc8d7b226b75d649f8f5fbd5a04af9196
Ivan Avraamov <avraamov@tadatuta.com>
2013-09-11 10:39:44 +0300

    add new mode for link-block a

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
 $(function(){
     $('.all').on('click', function(){
         $('.table__row_flow_income, .table__row_flow_outgo').css({display: 'table-row'});
+        $('.link-block a').removeClass('cashflow-curent');
+        $(this).addClass('cashflow-curent');
     });
     $('.income').on('click', function(){
         $('.table__row_flow_income').css({display: 'table-row'});
         $('.table__row_flow_outgo').css({display: 'none'});
+        $('.link-block a').removeClass('cashflow-curent');
+        $(this).addClass('cashflow-curent');
     });
     $('.outcome').on('click', function(){
         $('.table__row_flow_outgo').css({display: 'table-row'});
         $('.table__row_flow_income').css({display: 'none'});
+        $('.link-block a').removeClass('cashflow-curent');
+        $(this).addClass('cashflow-curent');
     });
 })
