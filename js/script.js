717dff2915db2a10f798f1d1c1bb82dc053d8c95
Ivan Avraamov <avraamov@tadatuta.com>
2013-09-11 10:45:58 +0300

    style and script fix for cashflow links

--- js/script.js
+++ js/script.js
@@ -3,17 +3,20 @@ $(function(){
         $('.table__row_flow_income, .table__row_flow_outgo').css({display: 'table-row'});
         $('.link-block a').removeClass('cashflow-curent');
         $(this).addClass('cashflow-curent');
+        $(this).removeClass('b-link_type_dashed');
     });
     $('.income').on('click', function(){
         $('.table__row_flow_income').css({display: 'table-row'});
         $('.table__row_flow_outgo').css({display: 'none'});
         $('.link-block a').removeClass('cashflow-curent');
         $(this).addClass('cashflow-curent');
+        $(this).removeClass('b-link_type_dashed');
     });
     $('.outcome').on('click', function(){
         $('.table__row_flow_outgo').css({display: 'table-row'});
         $('.table__row_flow_income').css({display: 'none'});
         $('.link-block a').removeClass('cashflow-curent');
         $(this).addClass('cashflow-curent');
+        $(this).removeClass('b-link_type_dashed');
     });
 })
