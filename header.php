c6cdbf87105585608969a6fba2e6b93af8d1b330
Ivan Avraamov <avraamov@tadatuta.com>
2013-09-19 18:11:04 +0300

    fix-13

--- header.php
+++ header.php
@@ -49,13 +49,11 @@
                 <?php } else{ ?>
                     <?php wp_list_categories("title_li=&include=6");?>
                 <?php } ?>
-                <?php if (false) { ?>
-                    <?php if ( is_category(7)) { ?>
-                          <!-- <li class="nav__item nav__item_state_current hidden">Деньги</li> -->
-                    <?php } else{ ?>
-                          <?php wp_list_categories("title_li=&include=7");?> 
-                    <?php } ?>
-                <?php } ?>
-                
+                /*
+                <?php if ( is_category(7)) { ?>
+                    <li class="nav__item nav__item_state_current hidden">Деньги</li>
+                <?php } else{ ?>
+                    <?php wp_list_categories("title_li=&include=7");?> 
+                */
                 <?php } ?>
             </ul>
