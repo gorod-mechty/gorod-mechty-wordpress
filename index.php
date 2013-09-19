c1670401a2164d9dd3718932fd36ba9f9c485cae
Ivan Avraamov <avraamov@tadatuta.com>
2013-09-19 17:26:57 +0300

    test fix-6 for frontpage post tumb

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
     <?php } else{ ?>
         <?php if ( is_category(1)|| in_category(6)|| in_category(7)) { ?>
             <div class="content">
-                <h1 class="heading heading_level_1"><?php $cat = get_the_category(); echo $cat[0]->cat_name; ?></h1>
                 <?php if ( is_category(1)) { ?>
+                <h1 class="heading heading_level_1">Новости</h1>
                     <ul class="news">
                         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
                             <li class="news__item" id="post-<?php the_ID(); ?>">
@@ -20,6 +20,7 @@
                         <?php posts_nav_link(' &#8212; ', __('&laquo; Newer Posts'), __('Older Posts &raquo;')); ?>
                     </ul>
                 <?php } else if ( is_category(6)){ ?>
+                <h1 class="heading heading_level_1"><?php $cat = get_the_category(); echo $cat[0]->cat_name; ?></h1>
                 <p><?php echo category_description(6); ?></p>
                     <div class="partners">
                         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
@@ -40,6 +41,7 @@
                         <?php posts_nav_link(' &#8212; ', __('&laquo; Newer Posts'), __('Older Posts &raquo;')); ?>
                     </div>
                 <?php } else if ( is_category(7)){ ?>
+                <h1 class="heading heading_level_1"><?php $cat = get_the_category(); echo $cat[0]->cat_name; ?></h1>
                 <p><?php echo category_description(7); ?></p>
                 <?php } ?>
             </div>
