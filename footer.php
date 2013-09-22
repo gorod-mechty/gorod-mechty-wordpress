14311f830988094246122d04cf94be1f815367fd
Ivan Avraamov <avraamov@tadatuta.com>
2013-09-22 22:11:00 +0300

    style fix-6

--- footer.php
+++ footer.php
@@ -8,11 +8,11 @@
                     <?php endwhile; ?>
                     <?php endif; ?>
                     <?php wp_reset_query(); ?>
-                </div>
-                <div class="footer__contacts">
-                    <div class="footer__text">
-                    © <?php echo date('Y'); ?> БО «Фонд Город Мечты»
-                    </div><a class="footer__link" href="mailto:info@gorod-mechty.org">info@gorod-mechty.org</a>
+                    <div class="footer__contacts">
+                        <div class="footer__text">
+                        © <?php echo date('Y'); ?> БО «Фонд Город Мечты»
+                        </div><a class="footer__link" href="mailto:info@gorod-mechty.org">info@gorod-mechty.org</a>
+                    </div>
                 </div>
                 <div class="socials">
                     <a class="socials__link socials__link_type_vk" href="http://vk.com/gorod.mechty">ВКонтакте</a><a class="socials__link socials__link_type_fb" href="/">FaceBook</a>
