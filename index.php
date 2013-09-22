bb81acbb7a76f478698c31a8dbeaa1a59973b3af
Ivan Avraamov <avraamov@tadatuta.com>
2013-09-22 19:20:54 +0300

    test fix for img default-4

--- index.php
+++ index.php
@@ -162,11 +162,12 @@
                                         $image_l = wp_get_attachment_image_src( $left_img, $size ); 
                                         $image_r = wp_get_attachment_image_src( $right_img, $size );
                                         ?>
-                                        <?php if (!get_field('right_img')) { ?>
-                                            <?php $image_r === '<?php bloginfo('template_url'); ?>/img/img2.php'; ?>
-                                        <?php } ?>
                                         <a href="<?php the_permalink() ?>"><img class="b-icon projects-columns__img-l" src="<?php echo $image_l[0]; ?>"/></a>
-                                        <a href="<?php the_permalink() ?>"><img class="b-icon projects-columns__img-r" src="<?php echo $image_r[0]; ?>"/></a>
+                                        <?php if ($image_r)) { ?>
+                                            <a href="<?php the_permalink() ?>"><img class="b-icon projects-columns__img-r" src="<?php echo $image_r[0]; ?>"/></a>
+                                        <?php } else { ?>
+                                            <a href="<?php the_permalink() ?>"><img class="b-icon projects-columns__img-r" src="<?php bloginfo('template_url'); ?>/img/img2.php"/></a>
+                                        <?php } ?>
                                     </div>
                                     <a class="b-link" href="<?php the_permalink() ?>"><a class="b-link" href="<?php the_permalink() ?>">
                                         <?php $title = get_the_title();
