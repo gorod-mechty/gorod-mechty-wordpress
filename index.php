932de8363d2d20f2e4e0a72d520bd5eccb105e79
Ivan Avraamov <avraamov@tadatuta.com>
2013-09-22 19:51:49 +0300

    test fix for img default-16

--- index.php
+++ index.php
@@ -162,9 +162,9 @@
                                         $image_l = wp_get_attachment_image_src( $left_img, $size ); 
                                         $image_r = wp_get_attachment_image_src( $right_img, $size );
                                         ?>
-                                        <a href="<?php the_permalink() ?>"><img class="b-icon projects-columns__img-l" src=
+                                        <a href="<?php the_permalink() ?>"><img class="b-icon projects-columns__img-l"
                                         <?php if (get_field('left_img')) { ?>
-                                            "<?php echo $image_l[0]; ?>"
+                                            src="<?php echo $image_l[0]; ?>"
                                         <?php } else { ?>
                                             <?php echo "<?php bloginfo('template_url'); ?>/img/img2.png" ?>
                                         <?php } ?>/></a>
@@ -172,7 +172,7 @@
                                         <?php if (get_field('right_img')) { ?>
                                             <?php echo $image_r[0]; ?>
                                         <?php } else { ?>
-                                            <?php echo "/uploads/img2.png" ?>
+                                             src="<?php bloginfo('template_url'); ?>/img/img2.png" ?>
                                         <?php } ?>/></a>
                                     </div>
                                     <a class="b-link" href="<?php the_permalink() ?>"><a class="b-link" href="<?php the_permalink() ?>">
