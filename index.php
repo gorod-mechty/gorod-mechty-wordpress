5a622fc94a5f5fd3ce142f2323c3e7416049976d
Ivan Avraamov <avraamov@tadatuta.com>
2013-09-22 19:43:17 +0300

    test fix for img default-11

--- index.php
+++ index.php
@@ -168,7 +168,12 @@
                                         <?php } else { ?>
                                             <?php echo "<?php bloginfo('template_url'); ?>/img/img2.png" ?>
                                         <?php } ?>/></a>
-                                        <a href="<?php the_permalink() ?>"><img class="b-icon projects-columns__img-r" src="<?php echo $image_r[0]; ?>"/></a>
+                                        <a href="<?php the_permalink() ?>"><img class="b-icon projects-columns__img-r" src=
+                                        <?php if (get_field('right_img')) { ?>
+                                            "<?php echo $image_r[0]; ?>"
+                                        <?php } else { ?>
+                                            <?php echo "<?php bloginfo('template_url'); ?>/img/img2.png" ?>
+                                        <?php } ?>/></a>
                                     </div>
                                     <a class="b-link" href="<?php the_permalink() ?>"><a class="b-link" href="<?php the_permalink() ?>">
                                         <?php $title = get_the_title();
